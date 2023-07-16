0d5f12496098685dce4f036f05ce9ebd6cf5257d
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-16 19:54:06 +0200

    test: test to check for valid save in repo

--- back-end/src/test/java/dev/cesar/backend/MediaFileRepositoryTest.java
+++ back-end/src/test/java/dev/cesar/backend/MediaFileRepositoryTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package dev.cesar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class MediaFileRepositoryTest {
-  
+
+    @Autowired
+    MediaFileRepository repo;
+
+    @Test
+    void saveShouldIncreaseTotalFilesInDBBy1() {
+        int count = repo.findAll().size();
+
+        repo.save(new MediaFile());
+
+        assertThat(repo.findAll().size()).isEqualTo(count + 1);
+    }
 }
