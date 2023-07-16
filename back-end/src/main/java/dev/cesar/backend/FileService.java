ddbaac162f44e99d2eecd633a042d724da427a44
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-16 17:50:16 +0200

    feat: established comunication between back-end and front-end

---
+++ back-end/src/main/java/dev/cesar/backend/FileService.java
@@ -0,0 +1,2 @@
+package dev.cesar.backend;public class FileService {
+}
--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,8 +1,27 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const data = await getData()
+
   return (
     <main>
+      {data}
     </main>
   )
 }
+
+import React from 'react'
+
+export async function getData() {
+  const res = await fetch("http://localhost:8080/api");
+  
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.text()
+}
+
+
+
