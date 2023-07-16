3bfe66ae65953970463671881f8d52c91edee6ed
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-16 17:52:58 +0200

    feat: implemented fileService methods

--- back-end/src/main/java/dev/cesar/backend/FileService.java
+++ back-end/src/main/java/dev/cesar/backend/FileService.java
@@ -1,2 +1,35 @@
-package dev.cesar.backend;public class FileService {
+package dev.cesar.backend;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileService {
+
+    private final Path staticPath = Paths.get("src/main/resources/static");
+
+    public Path store(String fileName, String content) throws IOException {
+        Path filePath = staticPath.resolve(fileName);
+        Files.write(filePath, content.getBytes());
+        return filePath;
+    }
+
+    public String read(String fileName) throws IOException {
+        Path filePath = staticPath.resolve(fileName);
+        return new String(Files.readAllBytes(filePath));
+    }
+
+    public void delete(String fileName) throws IOException {
+        Path filePath = staticPath.resolve(fileName);
+        Files.delete(filePath);
+    }
+
+    public boolean exists(String fileName) {
+        Path filePath = staticPath.resolve(fileName);
+        return Files.exists(filePath);
+    }
 }
