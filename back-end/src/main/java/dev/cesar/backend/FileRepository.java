813d3876163c0001db16d8df9efaba7da401fabb
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-16 19:52:45 +0200

    chore: renamed generic class name from File to MediaFile

--- back-end/src/main/java/dev/cesar/backend/FileRepository.java
+++
@@ -1,2 +0,0 @@
-package dev.cesar.backend;public class FileRepository {
-}
---
+++ back-end/src/main/java/dev/cesar/backend/MediaFileRepository.java
@@ -0,0 +1,8 @@
+package dev.cesar.backend;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FileRepository {
+    
+}
