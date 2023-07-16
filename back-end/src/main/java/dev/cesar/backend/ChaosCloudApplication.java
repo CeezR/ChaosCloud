7abf2aae34b25c561c1f4a91e6c99dd78e883168
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-16 16:02:53 +0200

    feat: initialized back-end

---
+++ back-end/src/main/java/dev/cesar/backend/ChaosCloudApplication.java
@@ -0,0 +1,13 @@
+package dev.cesar.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaosCloudApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/dev/cesar/backend/ChaosCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cesar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
