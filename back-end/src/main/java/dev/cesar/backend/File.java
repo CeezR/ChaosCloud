0c2d254a362b2535a34f1f9716013626e690e794
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-16 19:39:17 +0200

    feat: new file entity

--- back-end/src/main/java/dev/cesar/backend/File.java
+++ back-end/src/main/java/dev/cesar/backend/File.java
@@ -1,2 +1,19 @@
-package dev.cesar.backend;public class File {
+package dev.cesar.backend;
+
+import javax.persistence.*;
+
+@Entity
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
