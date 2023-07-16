ab42221354b9b8b73f7cdf80dfae2f7322f95ca0
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-16 17:53:21 +0200

    feat: test mapping added

--- back-end/src/main/java/dev/cesar/backend/ChaosCloudController.java
+++ back-end/src/main/java/dev/cesar/backend/ChaosCloudController.java
@@ -1,2 +1,18 @@
-package dev.cesar.backend;public class ChaosCloudController {
+package dev.cesar.backend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class ChaosCloudController {
+
+    @GetMapping
+    public ResponseEntity<String> getString() {
+        return ResponseEntity.ok("Hello World");
+    }
 }
