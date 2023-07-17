a634347de10d87ccf9540c5db732a23426360865
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-17 06:12:25 +0200

    test: test to check that save to repo return mediaFile obj with valid storageFileName

--- back-end/src/test/java/dev/cesar/backend/repository/MediaFileRepositoryTest.java
+++ back-end/src/test/java/dev/cesar/backend/repository/MediaFileRepositoryTest.java
@@ -1,10 +1,13 @@
 package dev.cesar.backend.repository;
 
 import dev.cesar.backend.MediaFile;
-import dev.cesar.backend.repository.MediaFileRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +24,25 @@ class MediaFileRepositoryTest {
         repo.save(new MediaFile());
 
         assertThat(repo.findAll().size()).isEqualTo(count + 1);
+    }
+
+    @Test
+    void successfulSaveShouldReturnMediaFile() {
+        MediaFile mediaFile = repo.save(new MediaFile());
+        assertThat(mediaFile).isNotNull();
+    }
+
+    @Test
+    void successfulSaveShouldReturnMediaFileWithStorageFileNameAsValidUUID() {
+        Pattern UUID_REGEX =
+                Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+
+        MediaFile mediaFile = repo.save(new MediaFile());
+
+        assertThat(mediaFile).isNotNull();
+        assertThat(mediaFile.getStorageFileName()).isNotNull();
+        assertThat(mediaFile.getStorageFileName());
+        assertThat(UUID_REGEX.matcher(mediaFile.getStorageFileName()).matches()).isTrue();
+
     }
 }
