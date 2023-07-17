92f0aed94472ad873aeaf4eaf883efd5f9724ce1
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-17 06:13:22 +0200

    feat: added new storageFileName to MediaFile entity

--- back-end/src/main/java/dev/cesar/backend/MediaFile.java
+++ back-end/src/main/java/dev/cesar/backend/MediaFile.java
@@ -1,6 +1,7 @@
 package dev.cesar.backend;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 public class MediaFile {
@@ -9,11 +10,17 @@ public class MediaFile {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    private String storageFileName = UUID.randomUUID().toString();
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getStorageFileName() {
+        return storageFileName;
     }
 }
