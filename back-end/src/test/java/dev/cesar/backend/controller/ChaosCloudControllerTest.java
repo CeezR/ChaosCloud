347891b739d9e608eaf97dd6e547304f51c8ec06
CeezR <cesarforthomme@gmail.com>
2023-08-02 08:46:36 +0200

    test: fix controller test not finding static folder

--- back-end/src/test/java/dev/cesar/backend/controller/ChaosCloudControllerTest.java
+++ back-end/src/test/java/dev/cesar/backend/controller/ChaosCloudControllerTest.java
@@ -2,6 +2,8 @@ package dev.cesar.backend.controller;
 
 import dev.cesar.backend.model.MediaFile;
 import dev.cesar.backend.model.MediaFileRequestDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,11 +14,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -34,6 +38,17 @@ class ChaosCloudControllerTest {
     RestTemplate restTemplate;
 
     private static final Path TEST_FILES_PATH = Paths.get("src/test/resources/testFiles");
+    private static final Path STATIC_PATH = Paths.get("src/test/resources/static");
+
+    @BeforeEach
+    void setUp() throws IOException {
+        Files.createDirectories(STATIC_PATH);
+    }
+
+    @AfterEach
+    void tearDown() {
+        FileSystemUtils.deleteRecursively(STATIC_PATH.toFile());
+    }
 
     @Test
     void testPostMapping() throws Exception {
