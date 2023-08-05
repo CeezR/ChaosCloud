3aae8700eabbcdc886770bac1470c1d88c12df4d
CeezR <cesarforthomme@gmail.com>
2023-08-05 11:37:01 +0200

    feat: implemented getAllFile mapping and logic

--- back-end/src/main/java/dev/cesar/backend/controller/ChaosCloudController.java
+++ back-end/src/main/java/dev/cesar/backend/controller/ChaosCloudController.java
@@ -2,23 +2,25 @@ package dev.cesar.backend.controller;
 
 import dev.cesar.backend.model.MediaFile;
 import dev.cesar.backend.model.MediaFileRequestDTO;
+import dev.cesar.backend.repository.MediaFileDBRepository;
 import dev.cesar.backend.service.MediaFileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class ChaosCloudController {
     private final MediaFileService service;
+    private final MediaFileDBRepository repository;
 
     @Autowired
-    public ChaosCloudController(MediaFileService service) {
+    public ChaosCloudController(MediaFileService service, MediaFileDBRepository repository) {
         this.service = service;
+        this.repository = repository;
     }
 
     @PostMapping(path = "/files")
@@ -29,5 +31,10 @@ public class ChaosCloudController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping(path = "/files")
+    public ResponseEntity<List<MediaFile>> getAllFiles() {
+        return ResponseEntity.ok(repository.findAll());
     }
 }
